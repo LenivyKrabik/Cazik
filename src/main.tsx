d4c53d2663e49672a8da9b9b99c4d41a64b87ff5
LenivyKrabik <chuvachok642@gmail.com>
2025-10-31 20:48:47 +0200

    Redacted total structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './homePage/home'
+
+const router = createBrowserRouter([
+  {path:"/" , element: <HomePage/>},
+  {path:"/game" , element:<></>},
+  {path:"/profile" , element:<></>}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
