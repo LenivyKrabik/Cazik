3cb3f0c98dc3ddc5f2b8f247a91febfed2e41c86
LenivyKrabik <chuvachok642@gmail.com>
2025-10-31 21:10:43 +0200

    Updated folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './homePage/home'
+import Games from './games/games';
+import Profile from './profile/profile';
 
 const router = createBrowserRouter([
   {path:"/" , element: <HomePage/>},
-  {path:"/game" , element:<></>},
-  {path:"/profile" , element:<></>}
+  {path:"/game" , element:<Games/>},
+  {path:"/profile" , element: <Profile/>}
 ]);
 
 createRoot(document.getElementById('root')!).render(
