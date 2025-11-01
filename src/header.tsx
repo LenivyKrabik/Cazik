b95d9c6efdc27a3f655f967cf6208ed0df618661
LenivyKrabik <chuvachok642@gmail.com>
2025-11-01 14:55:14 +0200

    Base for header done

--- src/header.tsx
+++ src/header.tsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
 import "./styles/header.css";
 
+//80px for logog 50 for navpan
+
 function Header() {
-  return <div className="header"></div>;
-}
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="../public/vite.svg"></img>
+      </div>
+      <div className="navBar">
+        <div className="buttonsPanel">
+          <Link to={"/"}>
+            <div className="navButton">Home</div>
+          </Link>
+          <Link to={"/profile"}>
+            <div className="navButton">Profile</div>
+          </Link>
+          <Link to={"/game"}>
+            <div className="navButton">PLAY</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+} //Need to change image placeholder (vite.svg)
 
 export default Header;
