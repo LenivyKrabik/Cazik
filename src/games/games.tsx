4ea535d0153a6ddc2adec96c24fa4887c79cd6a7
Lidonis-1 <prototup02@gmail.com>
2025-11-06 10:07:19 +0200

    now button can do smth

--- src/games/games.tsx
+++ src/games/games.tsx
@@ -1,10 +1,11 @@
 import Slots from "./slots/slots";
 import "../styles/games.css";
+import Mines from "./mines/mines_page";
 
 function Games() {
   return (
     <div>
-      <div className="gameWrapper"></div>
+      <div className="gameWrapper">{Mines()}</div>
     </div>
   );
 }
--- src/games/mines/mines_page.tsx
+++ src/games/mines/mines_page.tsx
@@ -5,10 +5,14 @@ import arr_matrix from "./mines_game";
 function Mines() {
   return (
     <div className="box">
-      {arr_matrix.map((line) => (
-        <div>
-          {line.map((item) => (
-            <button className="game-2_button" onClick={console.log(item)}>
+      {arr_matrix.map((line, index) => (
+        <div key={"" + index}>
+          {line.map((item, index_2) => (
+            <button
+              key={"" + index + "." + index_2}
+              className="game-2_button"
+              onClick={() => console.log(item)}
+            >
               {item}
             </button>
           ))}
